4ae1749345534a644fbee6905c1a2952682c8d72
John Johan Cresencio. San Juan <142964327+JohnJohan@users.noreply.github.com>
2024-04-18 11:13:44 +0800

    Update BankOffer.java

--- BankOffer.java
+++ BankOffer.java
@@ -1,7 +1,7 @@
 public class BankOffer{
     public static void main(String[] args){
 
-        SavingsAccount mySavings = new SavingsAccount("Cimb", 50000.0);
+        SavingsAccount mySavings = new SavingsAccount("Smart", 500.0);
 
         InterestCalculator calculator = new BankInterest();
         Insurance insurance = new BankInsurance();
@@ -13,14 +13,14 @@ public class BankOffer{
                             mySavings.accept(insurance,mySavings.getBalance()));
 
        //Seabank
-       mySavings.setBankName("SeaBank");
+       mySavings.setBankName("Globe");
        System.out.println("\nInterest of " + mySavings.getBankName() + " is " +
                            mySavings.accept(calculator,mySavings.getBalance()));
        System.out.println("Insurance of " + mySavings.getBankName() + " is " +
        mySavings.accept(insurance,mySavings.getBalance()));
 
        //GoTyme
-       mySavings.setBankName("GoTyme");
+       mySavings.setBankName("Ditto");
        System.out.println("\nInterest of " + mySavings.getBankName() + " is " +
                            mySavings.accept(calculator,mySavings.getBalance()));
        System.out.println("Insurance of " + mySavings.getBankName() + " is " +
